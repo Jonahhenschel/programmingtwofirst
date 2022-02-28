e179bc057f297d8806020cb9d4d3063802e420c4
jonahhenschel <jonahhenschel@gmail.com>
2022-02-28 09:09:26 -0600

    solved the tasks

---
+++ notes/notes feb 7th.py
@@ -0,0 +1,31 @@
+not feb 7 it is feb 8th
+
+q= input("what is your favorite sport?")
+if q in ["basketball", "hoops", "balling"]:
+    print("wow thats my favorite too")
+
+l=(1,2,3,4)
+11="abc"
+print(max(1))
+
+
+
+1=(2,3,4)
+sum1= sum(1)
+print(sum1)
+
+#list slicing is bassically taking a slice out of the list
+# it means you are taking a predetermined slice of the list
+list2=[50, 70, 30, 20,90,10]
+syntax: lst (initial[1:5])
+it would print 70-10
+
+.sort sorts it in ascending order or we can do .sort reverse=true it sorts in decsending order
+it can sort floasts alphbaetically or sorts ints by size3
+.reverse just reverses the list [1.2,3] would print 3 2 1
+extend function
+list1=[1,2]
+list2=[2,3]
+list2.extend(list1)
+print(str(list2))
+result 1,2,3,2
